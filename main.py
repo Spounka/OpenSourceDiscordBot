bb7460e202cc4c3bccdddd0a3f460b8f75f1363b
spounka <accelerator346@outlook.com>
2019-11-05 23:20:26 +0100

    fixed conflict between local and heroku

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import discord
 from discord.ext import commands
 from Scripts import custom_commands
-import env
+
+
+IS_HEROKU = False
+if IS_HEROKU:
+    import os
+    from boto.s3.connection import S3Connection
+else:
+    import env
+
+
+
 
 # secret token NjQwNTc5NjExMjYwMDkyNDc1.Xb76tQ.JMNdIQezzEfkEm81Ap-8Q_Poo9k
 
 client = commands.Bot(command_prefix="$")
 
 custom_commands.set_commands(client)
-
-client.run(env.SECRET_TOKEN)
+if IS_HEROKU:
+    s3 = S3Connection(os.environ['SECRET_TOKEN_KEY'])
+    client.run(s3)
+else:
+    client.run(env.SECRET_TOKEN)
 
