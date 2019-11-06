947689946a326c56c1942c87fe41fb2c36e36d6f
spounka <accelerator346@outlook.com>
2019-11-06 18:28:40 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import discord
 from discord.ext import commands
 from Scripts import custom_commands
-import env
 import os
 
 isHEROKU = True
-
 
 # secret token NjQwNTc5NjExMjYwMDkyNDc1.Xb76tQ.JMNdIQezzEfkEm81Ap-8Q_Poo9k
 
 client = commands.Bot(command_prefix="$")
 custom_commands.set_commands(client)
 
-if isHEROKU:
-    client.run(os.environ['SECRET_TOKEN'])
-else:
-    client.run(env.SECRET_TOKEN)
+client.run(os.environ['SECRET_TOKEN'])
 
