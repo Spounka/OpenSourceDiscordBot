a9cdf890d148e0e419ba2d12e3092955beb7c7a9
Spounka Von Schpeiner <accelerator346@outlook.com>
2020-04-19 16:12:44 +0100

    removed the secret token from code - Security Concerns

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import os
 
 isHEROKU = True
 
-# secret token NjQwNTc5NjExMjYwMDkyNDc1.Xb76tQ.JMNdIQezzEfkEm81Ap-8Q_Poo9k
-
 client = commands.Bot(command_prefix="$")
 custom_commands.set_commands(client)
 client.run(os.environ['SECRET_TOKEN'])
